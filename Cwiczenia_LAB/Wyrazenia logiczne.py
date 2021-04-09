8e5fdda89d6f2a9452043d0c0903ae4532d8a5cf
Folek90 <piotr.f@dianet.pl>
2021-04-09 15:22:53 +0200

    wyrazenia

---
+++ Cwiczenia_LAB/Wyrazenia logiczne.py
@@ -0,0 +1,17 @@
+import os
+
+path = r'c:\Users\user\documents\Cwiczenia_LAB\README.txt'
+
+#os.remove(path)
+
+'''
+if os.path.isfile(path):
+    print('File % exist', % path)
+else:
+    print('Creating a file %s' % path)
+    open(path,'x').close()
+    print('File %s created' % path)
+    '''
+
+result = os.path.isfile(path)
+print(result)
