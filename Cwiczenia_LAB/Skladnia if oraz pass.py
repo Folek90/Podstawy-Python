a4be0460e33cbc7ecba5058c137768af110e0e6e
Folek90 <piotr.f@dianet.pl>
2021-04-12 14:25:18 +0200

    skladnia if, pass oraz funkcja uproszczona

--- Cwiczenia_LAB/Skladnia if oraz pass.py
+++ Cwiczenia_LAB/Skladnia if oraz pass.py
@@ -1,4 +1,4 @@
-dayType = 1
+"""dayType = 1
 
 weekend = 1
 workday = 2
@@ -13,4 +13,25 @@ else:
 
 dayDescription = 'weekend' if dayType == 1 else 'workday' if dayType == 2 else 'holiday'
 #print(dayDescription)
-print(dayDescription)
+print(dayDescription) """
+
+# ZADANIE 1:
+
+
+price = 123
+bonus = 23
+bonus_granted = True
+
+""" if bonus_granted:
+    price -= bonus
+
+print(price) """
+
+price = price - bonus if bonus_granted else price 
+print(price) 
+
+# ZADANIE 2:
+rating = 4
+
+print('very good') if rating == 5 else print('good') if rating == 4 else print('weak')
+
