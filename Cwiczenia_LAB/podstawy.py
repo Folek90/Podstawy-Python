639b5c76ae7f1512db8846d432fa39e8f97394c5
Folek90 <piotr.f@dianet.pl>
2021-04-14 14:42:56 +0200

    kalkulator

--- Cwiczenia_LAB/podstawy.py
+++ Cwiczenia_LAB/podstawy.py
@@ -43,8 +43,22 @@ print(pow(4, 3))
 from math import *
 
 print(sqrt(7))
-
+'''
 name = input('Enter your name: ')
 age = input('Enter your age: ')
 Country = input('And where are you from? ')
 print('Hello '+ name + ', you are ' + age +' and you living in ' +Country+ ' so I see that\n it is country where Ziobro przesladuje Stonoge, is it right?')
+'''
+print('==========================================================')
+'''
+num1 = input("Please, enter a number: ")
+num2 = input("Please, enter another number: ")
+result = int(num1) + int(num2)
+print(result)
+'''
+print('==========================================================')
+
+num1 = input("Please, enter a number: ")
+num2 = input("Please, enter another one number: ")
+result = float(num1) / float(num2)
+print(result)
