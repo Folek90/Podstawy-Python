ddb86cbc8092451fe627af2c466de3daf272371f
Folek90 <piotr.f@dianet.pl>
2021-04-15 11:46:28 +0200

    podstawy tutorial yt

--- Cwiczenia_LAB/podstawy.py
+++ Cwiczenia_LAB/podstawy.py
@@ -36,7 +36,7 @@ print(255)
 my_num = 7
 print(str(my_num) + ' is my favorite numer')
 
-print(round(4.51))
+print(round(4.44))
 
 print(pow(4, 3))
 
@@ -49,14 +49,14 @@ age = input('Enter your age: ')
 Country = input('And where are you from? ')
 print('Hello '+ name + ', you are ' + age +' and you living in ' +Country+ ' so I see that\n it is country where Ziobro przesladuje Stonoge, is it right?')
 '''
-print('==========================================================')
+print('=========================================================')
 '''
 num1 = input("Please, enter a number: ")
 num2 = input("Please, enter another number: ")
 result = int(num1) + int(num2)
 print(result)
 '''
-print('==========================================================')
+print('=========================================================')
 '''
 # KALKULATOR 
 num1 = input("Please, enter a number: ")
@@ -64,13 +64,26 @@ num2 = input("Please, enter another one number: ")
 result = float(num1) / float(num2)
 print(result)'''
 
-
+'''
 color = input('Enter a color: ')
 plural_noun = input('Enter plural noun: ')
 celebrity = input('Enter a celebrity: ')
 print('Roses are ' + color)
 print(plural_noun + ' are yellow')
-print('I love ' + celebrity)
+print('I love ' + celebrity) 
+'''
 
-
-
+lucky_numbers = [4, 8, 15, 16, 23, 42]
+friends = ['Kevin', 'Karen', 'Jim', 'Tommy', 'Nikky']
+friends.sort()
+print(friends)
+lucky_numbers.sort()
+print(lucky_numbers)
+print('=========================================================')
+lucky_numbers.reverse()
+print(lucky_numbers)
+friends2 = friends.copy()
+friends2.extend(lucky_numbers)
+friends2.append("Leo")
+friends2.insert(0, "Kate")
+print(friends2)
